db41c497aad35cc85e2739afa761448bf3b2514c
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-27 14:19:41 +0100

    JOB01 à JOB03 OK

---
+++ JOB02.py
@@ -0,0 +1,11 @@
+def My_print_name(name):
+    # name = input('Entrez votre nom : ')
+    print(name)
+    
+My_print_name('Olivier')
+
+My_print_name('Andres')
+
+My_print_name('Abbas')
+
+My_print_name('John')
---
+++ JOB03.py
@@ -0,0 +1,8 @@
+def Add(a, b):
+    a = int(a)
+    b = int(b)
+    print(a + b)
+    
+Add(4, 5)
+
+Add(10, 20)
