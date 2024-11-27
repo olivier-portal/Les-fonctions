61a229c1b2607a052d2f13e10557ce1ab3e80a3a
OLIVIER PORTAL <olivier.portal@laplateforme.io>
2024-11-27 15:14:43 +0100

    modified JOB07

--- JOB07.py
+++ JOB07.py
@@ -3,12 +3,20 @@ def Are_U_A_Dev(language):
         return 'tu es un développeur web'
     elif language == 'Python' or language == 'python':
         return 'tu es un développeur IA'
+    elif language == 'Java' or language == 'java':
+        return 'tu es un développeur logiciel'
+    elif language == 'Reactjs' or language == 'reactjs':
+        return 'tu es un développeur mobile'
     else:
-        return 'tu es un OVNI'
+        return 'un jour, je serai le meilleur développeur…'
     
 print(Are_U_A_Dev('Python'))
 
 print(Are_U_A_Dev('Javascript'))
+
+print(Are_U_A_Dev('java'))
+
+print(Are_U_A_Dev('reactjs'))
 
 print(Are_U_A_Dev('C#'))
 
